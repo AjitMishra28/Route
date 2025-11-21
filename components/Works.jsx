6127c4cb7c0af24d5bf66939f9fe0a7516e5f216
Ajit Omkarnath Mishra <161081900+AjitMishra28@users.noreply.github.com>
2025-11-22 00:08:23 +0530

    Adjust WorkPage component styles for layout

--- components/Works.jsx
+++ components/Works.jsx
@@ -3,13 +3,14 @@ import { Settings, Atom, Code2, LayoutGrid, Users, Upload } from "lucide-react"
 
 export default function WorkPage() {
  return(
+
   <div
       className="
         absolute top-[1360px] left-[332px]
-        w-[1120px] h-[112px]
-        opacity-100
+        w-[1190px] h-[112px]
+        opacity-300
         flex items-center justify-between gap-[10px]
-        px-[99px] py-[42px]
+        px-[99px] py-[92px]
         text-white font-semibold
         rounded-xl shadow-md
       "
